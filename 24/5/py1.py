0fe4c2d9e2af9ce3fe1c905624a112f2fab12766
fabianstw <fstiewe@aol.com>
2024-12-05 09:13:06 +0100

    Beautiful is different but bruteforce works

---
+++ 24/5/py1.py
@@ -0,0 +1,42 @@
+rules = {}
+messages = []
+
+addRule = True
+
+with open('inp.txt', 'r') as file:
+	data = file.readlines()
+	for line in data:
+		if line == '\n':
+			addRule = False
+			continue
+		if addRule:
+			rule = line.strip().split('|')
+			if rule[0] in rules:
+				rules[rule[0]].append(rule[1])
+			else:
+				rules[rule[0]] = [rule[1]]
+		else:
+			messages.append(line.strip().split(","))
+			
+
+def checkMessage(rule, message):
+	print(message)
+	for i in range(len(message) - 1):
+		for j in range(i + 1, len(message)):
+			if message[i] not in rule:
+				return False
+			if message[j] not in rule[message[i]]:
+				return False
+	return True
+
+
+def checkAllMessages(rule, messages):
+	res = 0
+	for message in messages:
+		if checkMessage(rule, message):
+			# add the middle element of message to res
+			res += int(message[len(message) // 2])
+	return res
+	
+	
+print(checkAllMessages(rules, messages))
---
+++ 24/5/py2.py
@@ -0,0 +1,55 @@
+rules = {}
+messages = []
+
+addRule = True
+
+with open('inp.txt', 'r') as file:
+	data = file.readlines()
+	for line in data:
+		if line == '\n':
+			addRule = False
+			continue
+		if addRule:
+			rule = line.strip().split('|')
+			if rule[0] in rules:
+				rules[rule[0]].append(rule[1])
+			else:
+				rules[rule[0]] = [rule[1]]
+		else:
+			messages.append(line.strip().split(","))
+
+
+def checkMessage(rule, message):
+	print(message)
+	for i in range(len(message) - 1):
+		for j in range(i + 1, len(message)):
+			if message[i] not in rule:
+				return False
+			if message[j] not in rule[message[i]]:
+				return False
+	return True
+
+
+def fixMessage(rules, message):
+	newMessage = []
+	l = len(message)
+	for j in range(l):
+		for i in range(len(message)):
+			if message[i] in rules:
+				if all(x in rules[message[i]] for x in message if x != message[i]):
+					newMessage.append(message[i])
+					message.pop(i)
+					break
+	return newMessage
+
+
+def checkAllMessages(rule, messages):
+	res = 0
+	for message in messages:
+		if not checkMessage(rule, message):
+			message = fixMessage(rules, message)
+			res += int(message[len(message) // 2])
+	return res
+
+
+print(checkAllMessages(rules, messages))
