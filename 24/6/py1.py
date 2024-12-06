ad00b0b30bcc9261d9142a0b51ade9cd306055af
fabianstw <fstiewe@aol.com>
2024-12-06 15:06:46 +0100

    Bruteforce is a good force

---
+++ 24/6/py1.py
@@ -0,0 +1,40 @@
+dmap = []
+currPos = (-1, -1)
+
+with open('inp.txt', 'r') as file:
+	data = file.readlines()
+	for i in range(len(data)):
+		newLine = []
+		for j in range(len(data[i])):
+			if data[i][j] != "\n":
+				newLine.append(data[i][j])
+			if data[i][j] == "^":
+				currPos = (i, j)
+		dmap.append(newLine)
+
+
+def moving(d_map, pos):
+	directions = {"N": (-1, 0), "E": (0, 1), "S": (1, 0), "W": (0, -1)}
+	d_order = ["N", "E", "S", "W"]
+	d = "N"
+	
+	while True:
+		dx, dy = directions[d]
+		new_pos = (pos[0] + dx, pos[1] + dy)
+		
+		if not (0 <= new_pos[0] < len(d_map) and 0 <= new_pos[1] < len(d_map[0])):
+			break
+		
+		if d_map[new_pos[0]][new_pos[1]] == "#":
+			d = d_order[(d_order.index(d) + 1) % 4]
+		else:
+			pos = new_pos
+			d_map[pos[0]][pos[1]] = "X"
+	
+	for row in d_map:
+		print(row)
+	
+	return sum(row.count("X") for row in d_map)
+
+
+print(moving(dmap, currPos))
---
+++ 24/6/py2.py
@@ -0,0 +1,78 @@
+import copy
+
+# too high 2140
+
+dmap = []
+initPos = (-1, -1)
+
+with open('inp.txt', 'r') as file:
+	data = file.readlines()
+	for i in range(len(data)):
+		newLine = []
+		for j in range(len(data[i])):
+			if data[i][j] != "\n":
+				newLine.append(data[i][j])
+			if data[i][j] == "^":
+				initPos = (i, j)
+		dmap.append(newLine)
+
+
+directions = {"N": (-1, 0), "E": (0, 1), "S": (1, 0), "W": (0, -1)}
+d_order = ["N", "E", "S", "W"]
+
+
+def doesLoop(d_map, pos, dir):
+	visited = set()
+	
+	while True:
+		if (pos, dir) in visited:
+			return 1
+		visited.add((pos, dir))
+		
+		dx, dy = directions[dir]
+		new_pos = (pos[0] + dx, pos[1] + dy)
+		
+		if not (0 <= new_pos[0] < len(d_map) and 0 <= new_pos[1] < len(d_map[0])):
+			return 0
+		if d_map[new_pos[0]][new_pos[1]] == "#":
+			dir = d_order[(d_order.index(dir) + 1) % 4]
+		else:
+			pos = new_pos
+
+
+usedPos = set()
+
+def moving(d_map, pos):
+	d = "N"
+	loop_res = 0
+	
+	while True:
+		dx, dy = directions[d]
+		new_pos = (pos[0] + dx, pos[1] + dy)
+		
+		if not (0 <= new_pos[0] < len(d_map) and 0 <= new_pos[1] < len(d_map[0])):
+			break
+		
+		if d_map[new_pos[0]][new_pos[1]] == "#":
+			d_map[pos[0]][pos[1]] = "+"
+			d = d_order[(d_order.index(d) + 1) % 4]
+		else:
+			# create a deepcopy of d_map
+			if new_pos != initPos and new_pos not in usedPos:
+				usedPos.add(new_pos)
+				d_map_copy = copy.deepcopy(d_map)
+				d_map_copy[new_pos[0]][new_pos[1]] = "#"
+				loop_res += doesLoop(d_map_copy, pos, d_order[(d_order.index(d) + 1) % 4])
+			pos = new_pos
+			if d == "N" or d == "S":
+				d_map[pos[0]][pos[1]] = "|"
+			else:
+				d_map[pos[0]][pos[1]] = "-"
+				
+	for row in d_map:
+		print(row)
+	
+	return loop_res
+
+
+print(moving(dmap, initPos))
